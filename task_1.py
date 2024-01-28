7482a6be75c8668ccf08670f7173a140ab195417
SanaIslam01 <sanaislam267@gmail.com>
2024-01-28 15:12:05 +0500

    new msg

---
+++ task_1.py
@@ -0,0 +1,15 @@
+# task 1
+name=input("enter your name")
+age=input("enter your age")
+print("Hello",name,"your age is",age)
+
+# task 2
+celsius = int(input("enter temperature in Celsius "))
+conversion = celsius*9/5+32
+print("now the temperature is in foran",conversion)
+
+# task 3
+h=float(input("enter your height"))
+w=float(input("enter your weight"))
+bmi=w/h**2
+print("your bmi is now hello",bmi)
